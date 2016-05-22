f6edbf52e5514ada153151aa97171091e5086bb8
刘其深 <1145694956@qq.com>
2016-05-22 22:56:29 +0800

    投资列表页加入tab组件和group组件

--- app/js/app.js
+++ app/js/app.js
@@ -22,6 +22,7 @@ const App = React.createClass({
       children,
       ...props,
       } = this.props;
+      console.log("app props:",this.props);
     const transition = children.props.transition || 'sfr';
 
     return (
--- app/js/pages/investmentIndex.js
+++ app/js/pages/investmentIndex.js
@@ -2,15 +2,74 @@ import React from 'react';
 import {
   Container,
   Group,
+  Grid,
+  Col,
+  Tabs
 } from 'amazeui-touch';
 
+const Group_header=React.createClass({
+    render() {
+      return (
+          <h5>{this.props.title}</h5>
+      )
+    }
+    
+});
+
+const Group_body =React.createClass({
+    render(){
+      return (
+          <Grid collapse={true}>
+            <Col cols={1}>
+                <div className="investmentCard-subtitle">年化收益</div>
+                <div>
+                  <span className="investmentCard-yearRate-number">8.0</span>
+                  <span className="investmentCard-yearRate-unit">%</span>
+                </div>
+            </Col>
+            <Col cols={3}>
+                <div className="text-center investmentCard-subtitle">投资期限</div>
+                <div className="text-center">
+                  <span className="investmentCard-deadline-number">1</span>
+                  <span className="investmentCard-deadline-unit">个月</span>
+                </div>
+            </Col>
+            <Col cols={2}>
+                <div className="text-center investmentCard-subtitle">可购金额</div>
+                <div className="text-center">
+                  <span className="investmentCard-remainAmount-number">100.5</span>
+                  <span className="investmentCard-remainAmount-unit">万</span>
+                </div>
+            </Col>
+          </Grid>
+      )
+    }
+});
 const InvestmentIndex = React.createClass({
   render() {
     return (
       <Container {...this.props}>
-        <Group>
-          <h2>这是理财列表页</h2>
-        </Group>
+        <Tabs defaultActiveKey={0}>
+            <Tabs.Item title="理财计划" navStyle="success">
+              <Group header="">
+                <Group_header title="天天赚"/>
+                <Group_body />
+              </Group>   
+            </Tabs.Item>
+            <Tabs.Item title="项目直投" navStyle="success">
+              <Group header="">
+                <Group_header title="天天赚"/>
+                <Group_body />
+              </Group>   
+            </Tabs.Item>
+            <Tabs.Item title="债权转让" navStyle="success">
+              <Group header="">
+                <Group_header title="天天赚"/>
+                <Group_body />
+              </Group>   
+            </Tabs.Item>
+        </Tabs>
+        
       </Container>
     );
   },
